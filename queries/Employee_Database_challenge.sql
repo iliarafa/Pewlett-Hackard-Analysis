33ed52593b8db12fabc6b367fe0c2ea81b94fc88
iliarafa <83038866+iliarafa@users.noreply.github.com>
2021-08-01 12:10:25 +0300

    mentorship edit

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -66,12 +66,14 @@ SELECT DISTINCT ON (employees.emp_no)employees.emp_no,
 	   employees_info.from_date,
 	   employees_info.to_date,
 	   titles.title
-INTO mentorship_eligibility
+INTO mentorship
 FROM employees
 INNER JOIN employees_info ON employees.emp_no = employees_info.emp_no
-INNER JOIN titles ON employees.emp_no = employees_info.emp_no
+INNER JOIN titles ON titles.emp_no = employees_info.emp_no
 WHERE (employees.birth_date BETWEEN '1965/1/1' AND '12/31/1965')
 AND (employees_info.to_date = '9999/01/01')
 ORDER BY emp_no;
 
-SELECT * FROM mentorship_eligibility
+
+SELECT title, COUNT(*) FROM mentorship
+GROUP BY title
